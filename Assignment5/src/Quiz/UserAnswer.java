e148083a2163178f69831b6271f3b029f4da19af
Tanakan Sojisirikul <tas199@usask.ca>
2024-11-05 19:22:54 -0600

    implemented class UserAnswer
    # this class hold user input
    
    # passes regression test

---
+++ Assignment5/src/Quiz/UserAnswer.java
@@ -0,0 +1,43 @@
+package Quiz;
+
+public class UserAnswer
+{
+    private int answer;
+
+    public UserAnswer(int userInput)
+    {
+    this.answer = userInput;
+
+    }
+
+    public int getAnswer()
+    {
+        return answer;
+
+    }
+
+    public String toString()
+    {
+        String userAnswerString;
+
+        userAnswerString = "";
+        userAnswerString += "User answer: " + this.answer + "\n";
+
+        return userAnswerString;
+
+    }
+
+    public static void main(String[] args)
+    {
+        UserAnswer test = new UserAnswer(3);
+
+        if (test.getAnswer() != 3)
+        {
+            System.out.println("Error: answer attribute not match with the input argument");
+        }
+
+        System.out.println(test);
+    }
+
+
+}
