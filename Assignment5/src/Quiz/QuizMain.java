4ff23ebbf1270bc24fee4b0d2c476ca936664dd5
Tanakan Sojisirikul <tas199@usask.ca>
2024-11-06 01:23:45 -0600

    implemented getScore(), toString()
    # passed the tests

--- Assignment5/src/Quiz/QuizMain.java
+++ Assignment5/src/Quiz/QuizMain.java
@@ -28,11 +28,15 @@ public class QuizMain
 
     }
 
-    public void addQuestion(String question, String[] choices, int questionAnswer, int userAnswer)
+    public void addQuestion(String question, String[] choices, int quiznAnswer, int userAnswer)
     {
-        this.questionList.add(new QuizQuestion(question, choices, questionAnswer));
+        this.questionList.add(new QuizQuestion(question, choices, quiznAnswer));
         this.userAnswerList.add(new UserAnswer(userAnswer));
         this.questionIdx++;
+        if (quiznAnswer == userAnswer)
+        {
+            this.score++;
+        }
     }
 
     public QuizQuestion getQuestion(int questionNumber)
@@ -53,7 +57,7 @@ public class QuizMain
 
     }
 
-    public int getQuestionTotal()
+    public int getQuizTotal()
     {
         return this.questionList.size();
 
@@ -61,7 +65,28 @@ public class QuizMain
 
     public boolean isCorrect(int questionNumber)
     {
-        return getQuizAnswer(questionNumber) == getUserAnswer(questionNumber);
+        return this.getQuizAnswer(questionNumber) == this.getUserAnswer(questionNumber);
+    }
+
+    public int getScore()
+    {
+        return this.score;
+
+    }
+
+    public String toString()
+    {
+        String quizString;
+
+        quizString = "";
+
+        for (int i = 0; i < this.getQuizTotal(); i++)
+        {
+            quizString += "Question " + (i+1) + ": " + this.isCorrect(i) + "\n";
+        }
+        quizString += "Score: " + this.getScore() + "/" + this.getQuizTotal() + "\n";
+
+        return quizString;
     }
 
     /*
@@ -99,9 +124,10 @@ public class QuizMain
         String[] choiceTest = {"1. Fire", "2. Water", "3. Earth", "4. Wind"};
 
         QuizMain test = new QuizMain();
-        test.addQuestion("What is Azula element in the Avatar animation?", choiceTest, 1, 1);
+        test.addQuestion("What is Azula nation Avatar animation?",
+                choiceTest, 1, 1);
 
-        if (test.getQuestionTotal() != 1)
+        if (test.getQuizTotal() != 1)
         {
             System.out.println("Error addQuestion(): incorrect number of size in QuizMain List after added a quiz");
         }
@@ -123,9 +149,17 @@ public class QuizMain
             System.out.println("Error isCorrect(): incorrect boolean result");
         }
 
+        test.addQuestion("What is Aang nation Avatar animation?",
+                choiceTest, 4, 1);
+
+        if (test.getScore() != 1)
+        {
+            System.out.println("Error getScore(): incorrect score result");
+        }
 
 
-        //System.out.println(test.toString());
+
+        System.out.println(test.toString());
 
 
     }
