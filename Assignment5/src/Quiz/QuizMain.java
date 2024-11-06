fd595c7c02a946f2f5da4613315b98905b82a2c0
Tanakan Sojisirikul <tas199@usask.ca>
2024-11-06 00:54:08 -0600

    implemented construct, getQuizAnswer(), getUserAnswer(), getQuestionTotal()
    # passed the tests

--- Assignment5/src/Quiz/QuizMain.java
+++ Assignment5/src/Quiz/QuizMain.java
@@ -5,30 +5,63 @@ Student Course: 11175553
 Course: CMPT270-03
 */
 
-/*
+
 package Quiz;
 
-import java.util.LinkedList;
+import java.util.ArrayList;
+import java.util.List;
 
-public abstract class QuizMain
+public class QuizMain
 {
-    private LinkedList<QuizQuestion> questionList;
-
-
+    private List<QuizQuestion> questionList;
+    private List<UserAnswer> userAnswerList;
+    private int questionIdx;
     private int score;
 
     public QuizMain()
     {
-        this.questionList = new LinkedList<QuizQuestion>();
+        this.questionList = new ArrayList<QuizQuestion>();
+        this.userAnswerList = new ArrayList<UserAnswer>();
+        this.questionIdx = 0;
         this.score = 0;
+
 
     }
 
-    public abstract QuizQuestion addQuestion(String question, String choice, String answer);
+    public void addQuestion(String question, String[] choices, int questionAnswer, int userAnswer)
     {
-        return null;
+        this.questionList.add(new QuizQuestion(question, choices, questionAnswer));
+        this.userAnswerList.add(new UserAnswer(userAnswer));
+        this.questionIdx++;
+    }
 
+    public QuizQuestion getQuestion(int questionNumber)
+    {
+        // catching null
+        return this.questionList.get(questionNumber);
+    }
 
+    public int getQuizAnswer(int questionNumber)
+    {
+        return this.questionList.get(questionNumber).getAnswer();
+
+    }
+
+    public int getUserAnswer(int questionNumber)
+    {
+        return this.userAnswerList.get(questionNumber).getAnswer();
+
+    }
+
+    public int getQuestionTotal()
+    {
+        return this.questionList.size();
+    }
+
+    /*
+    public UserAnswer getUserAnswer(int questionNumber)
+    {
+        return this.userAnswerList.get(questionNumber);
 
     }
 
@@ -38,9 +71,15 @@ public abstract class QuizMain
 
     }
 
-    public abstract QuizQuestion getNextQuestion();
+    public int getQuestionTotal()
     {
-        return this.questionList.next();
+        return this.questionList.size();
+    }
+
+    public int[] getUserAnswer()
+    {
+        return this
+
     }
 
     public String toString()
@@ -48,5 +87,36 @@ public abstract class QuizMain
         return "";
     }
 
+    */
+    public static void main(String[] args)
+    {
+        String[] choiceTest = {"1. Fire", "2. Water", "3. Earth", "4. Wind"};
+
+        QuizMain test = new QuizMain();
+        test.addQuestion("What is Azula element in the Avatar animation?", choiceTest, 1, 1);
+
+        if (test.getQuestionTotal() != 1)
+        {
+            System.out.println("Error addQuestion(): incorrect number of size in QuizMain List after added a quiz");
+        }
+
+        if (test.getQuizAnswer(0) != 1)
+        {
+            System.out.println("Error getQuizAnswer(): incorrect return number");
+
+        }
+
+        if (test.getUserAnswer(0) != 1)
+        {
+            System.out.println("Error getUserAnswer(): incorrect return number");
+
+        }
+
+
+
+        //System.out.println(test.toString());
+
+
+    }
+
 }
-*/
