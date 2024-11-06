a56f060f22d4319f9bceb52a52cea6c6d1469344
Tanakan Sojisirikul <tas199@usask.ca>
2024-11-05 18:34:59 -0600

    commit draft of QuizMain

---
+++ Assignment5/src/Quiz/QuizMain.java
@@ -0,0 +1,52 @@
+/*
+Name: Sojisirikul, Tanakan
+NSID: tas199
+Student Course: 11175553
+Course: CMPT270-03
+*/
+
+/*
+package Quiz;
+
+import java.util.LinkedList;
+
+public abstract class QuizMain
+{
+    private LinkedList<QuizQuestion> questionList;
+
+
+    private int score;
+
+    public QuizMain()
+    {
+        this.questionList = new LinkedList<QuizQuestion>();
+        this.score = 0;
+
+    }
+
+    public abstract QuizQuestion addQuestion(String question, String choice, String answer);
+    {
+        return null;
+
+
+
+    }
+
+    public int getScore()
+    {
+        return this.score;
+
+    }
+
+    public abstract QuizQuestion getNextQuestion();
+    {
+        return this.questionList.next();
+    }
+
+    public String toString()
+    {
+        return "";
+    }
+
+}
+*/
