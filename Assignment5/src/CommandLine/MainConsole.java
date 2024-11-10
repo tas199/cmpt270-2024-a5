39a67dc81963976b2cd7e7519beefa649dedad2c
Tanakan Sojisirikul <tas199@usask.ca>
2024-11-09 23:34:45 -0600

    modified the console to associate to quiz and reading class only

--- Assignment5/src/CommandLine/MainConsole.java
+++ Assignment5/src/CommandLine/MainConsole.java
@@ -1,13 +1,9 @@
-
 package CommandLine;
 
-import GradeQuiz.QuizQuestion;
-import GradeQuiz.QuizFileReader;
-import GradeQuiz.QuizMain;
+import Quiz.QuizFileReader;
+import Quiz.Quiz;
 
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class MainConsole
@@ -15,44 +11,35 @@ public class MainConsole
     public static void main(String[] args)
     {
         QuizFileReader reader = new QuizFileReader();
-        List<QuizQuestion> quizDisplay = new ArrayList<>();
-        QuizMain scoreCollect = new QuizMain();
+        Quiz quiz = new Quiz();
         Scanner sc = new Scanner(System.in);
         String filepath;
-        int i;
+        int idx;
         int userAnswer;
-        String question;
-        String[] choice;
-        int answer;
 
         filepath = "Assignment5/src/input.txt";
         try
         {
-            quizDisplay = reader.readQuiz(filepath);
+            quiz = reader.readQuiz(filepath);
 
         } catch (FileNotFoundException e)
         {
             System.out.println("The file not found");
         }
 
-        i = 0;
-        while (i < quizDisplay.size())
+        idx = 0;
+        while (idx < quiz.getQuizTotal())
         {
-            System.out.println("\nQuestion #" + (i+1));
-            System.out.print(quizDisplay.get(i));
+            System.out.println("\nQuestion " + (idx+1) + ":");
+            System.out.print(quiz.getQuestion(idx));
             System.out.print("Enter your answer (1-4): ");
 
             userAnswer = sc.nextInt();
-
-            question = quizDisplay.get(i).getQuestion();
-            choice = quizDisplay.get(i).getChoice();
-            answer = quizDisplay.get(i).getAnswer();
-
-            scoreCollect.addQuestion(question, choice, answer, userAnswer);
-            i++;
+            quiz.addUserAnswer(userAnswer);
+            idx++;
 
         }
-        System.out.println(scoreCollect);
+        System.out.println("\n" + quiz);
 
     }
 }
